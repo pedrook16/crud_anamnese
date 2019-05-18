25945c004f7e2a898a15dbed5a897352ef5cf2eb
Pedro Henrique <pedrook16@gmail.com>
2019-05-18 00:09:41 -0300

    table home

--- Views/home.php
+++ Views/home.php
@@ -1,1 +1,32 @@
-Hello World
+<div class="container">
+<div class="box">
+    <div class="box-header">     
+      <div class="box-tools">
+        <a href="<?php echo BASE_URL.'add'; ?>" class="btn btn-success">Adicionar</a>
+      </div>
+    </div>
+    <div class="box-body">
+      <table class="table">
+        <tr>
+          <th>Código</th>
+          <th>Anamnese</th>
+          <th>Resposta</th>
+          <th width="130">Ações</th>
+        </tr>
+        	<tr>
+        		<td>1</td>
+                <td>Tenho</td>
+                <td>sim</td>
+        		<td>
+        			<div class="btn-group">
+                <a href="<?php echo BASE_URL.'view'; ?>" class="btn btn-xs btn-primary"><i class="fas fa-binoculars"></i></a>
+                <a href="<?php echo BASE_URL.'edit'; ?>" class="btn btn-xs btn-warning"><i class="far fa-edit"></i></a>
+                <a href="<?php echo BASE_URL.'trash'; ?>" class="btn btn-xs btn-danger"><i class="fas fa-trash-alt"></i></a>
+				    	</div>
+        		</td>
+        	</tr>
+      
+
+      </table>
+
+</div>
--- Views/template.php
+++ Views/template.php
@@ -1,9 +1,17 @@
 <html>
   <head>
-    <title>Site</title>
+    <title>Sistema Anamnese</title>
+    <link rel="shortcut icon" href="<?php echo BASE_URL; ?>assets/img/favicon.png" type="image/x-icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=PT+Serif&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/style.css">
   </head>
-  <body>
-    <h1>Site</h1>
+  <body class="container">
+    <div class="container">
+      <h1>Listagem de Anamneses</h1>
+    </div>
 
     <?php $this->loadViewInTemplate($viewName, $viewData); ?>
   </body>
